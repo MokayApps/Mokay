c2c75d28a5eb3ad2ef387af92dc654b79b71af7a
Andrei Kozlov <akozlov@MacBook-Pro-de-Andrei.local>
2024-12-04 15:33:00 +0100

    Created Database

---
+++ Mokay/Mokay/Database/Database.swift
@@ -0,0 +1,51 @@
+//
+//  Database.swift
+//  MokayExample
+//
+//  Created by Andrei Kozlov on 4/12/24.
+//
+
+import SwiftData
+import Foundation
+
+@ModelActor
+public actor Database {
+
+	public init(versionedSchema: VersionedSchema.Type, configurations: ModelConfiguration) throws {
+		let schema = Schema(versionedSchema: versionedSchema)
+		let modelContainer = try ModelContainer(for: schema, configurations: configurations)
+		let modelContext = ModelContext(modelContainer)
+		modelContext.autosaveEnabled = false
+		self.modelExecutor = DefaultSerialModelExecutor(modelContext: modelContext)
+		self.modelContainer = modelContainer
+	}
+
+	public func fetch<Model: PersistentModel>(predicate: Predicate<Model>) throws -> [Model] {
+		let fetchDescriptor = FetchDescriptor<Model>(predicate: predicate)
+		return try modelContext.fetch(fetchDescriptor)
+	}
+
+	public func insert(model: some PersistentModel) {
+		modelContext.insert(model)
+	}
+
+	public func delete(model: some PersistentModel) {
+		modelContext.delete(model)
+	}
+
+	public func delete<Model: PersistentModel>(type: Model.Type, predicate: Predicate<Model>, includeSubsclasses: Bool) throws {
+		try modelContext.delete(model: type, where: predicate, includeSubclasses: includeSubsclasses)
+	}
+
+	/// Update properties of SwiftData models.
+	///
+	/// Use this to update models in any thread. Don't modify models without this method.
+	public func update(updateBlock: () -> Void) {
+		updateBlock()
+	}
+
+	public func save() throws {
+		try modelContext.save()
+	}
+
+}
