f8ee95c7fbb07c9ec7deb240e9bc43bdb64aa96d
Egor Privalov <eprivalovdev@gmail.com>
2025-01-17 17:49:27 +0200

    fixed paddings

--- Mokay/Mokay/DesignKit/Header.swift
+++ Mokay/Mokay/DesignKit/Header.swift
@@ -8,51 +8,51 @@
 import SwiftUI
 
 struct HeaderView: View {
-    
-    let text: String
-    let color: Color
-    let style: TypographyStyle
-    
-    var body: some View {
-        Text(text)
-            .typography(type: style)
-            .foregroundStyle(color)
-            .padding([.leading, .bottom], 8)
-    }
-    
+	
+	let text: String
+	let color: Color
+	let style: TypographyStyle
+	
+	var body: some View {
+		Text(text)
+			.typography(type: style)
+			.foregroundStyle(color)
+			.padding([.leading, .bottom], .x1)
+	}
+	
 }
 
 private struct ExampleContentView: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Spacer()
-            HeaderView(
-                text: "Capture",
-                color: .blue,
-                style: .caption
-            )
-            HeaderView(
-                text: "Header 4",
-                color: .blue,
-                style: .h4
-            )
-            HeaderView(
-                text: "Header 2",
-                color: .blue,
-                style: .h2
-            )
-            HeaderView(
-                text: "Description",
-                color: .blue,
-                style: .description
-            )
-            Spacer()
-        }
-    }
+	var body: some View {
+		VStack(alignment: .leading, spacing: .x2) {
+			Spacer()
+			HeaderView(
+				text: "Capture",
+				color: .blue,
+				style: .caption
+			)
+			HeaderView(
+				text: "Header 4",
+				color: .blue,
+				style: .h4
+			)
+			HeaderView(
+				text: "Header 2",
+				color: .blue,
+				style: .h2
+			)
+			HeaderView(
+				text: "Description",
+				color: .blue,
+				style: .description
+			)
+			Spacer()
+		}
+	}
 }
 
 struct ExampleContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ExampleContentView()
-    }
+	static var previews: some View {
+		ExampleContentView()
+	}
 }
