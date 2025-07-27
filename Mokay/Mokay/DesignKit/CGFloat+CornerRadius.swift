606683b30c125275995bc0aecad2a0860f97d36a
Egor Privalov <eprivalovdev@gmail.com>
2025-07-27 13:44:33 +0300

    added corner radius

---
+++ Mokay/Mokay/DesignKit/CGFloat+CornerRadius.swift
@@ -0,0 +1,17 @@
+//
+//  CGFloat+CornerRadius.swift
+//  Mokay
+//
+//  Created by Єгор Привалов on 27.07.2025.
+//
+
+import Foundation
+
+extension CGFloat {
+	public static let r05: CGFloat = 4
+	public static let r1: CGFloat = 8
+	public static let r2: CGFloat = 16
+	public static let r3: CGFloat = 24
+	public static let r4: CGFloat = 32
+	public static let r5: CGFloat = 40
+}
