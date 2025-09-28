81f528d9afa64a3ec8df4a39ca4481b3890ed407
Roman Apanasevich <roman.apanasevich@ironwaterstudio.com>
2025-09-28 15:01:27 +0300

    Added color in resources and method for корретка

--- Mokay/Mokay/DesignKit/InputView/InputView.swift
+++ Mokay/Mokay/DesignKit/InputView/InputView.swift
@@ -8,144 +8,153 @@
 import SwiftUI
 
 public struct InputView<LeadingContent: View, TrailingContent: View>: View {
-    
-    @Environment(\.inputViewStyle) var inputViewStyle
+	
+	@Environment(\.inputViewStyle) var inputViewStyle
 	
 	@Binding var text: String
 	let placeholder: String
 	let shouldChange: @MainActor (String) -> Bool
-    let leadingItem: LeadingContent
-    let trailingItem: TrailingContent
-    
-    public init(
-        text: Binding<String>,
-        placeholder: String,
-        @ViewBuilder leadingItem: () -> LeadingContent,
-        @ViewBuilder trailingItem: () -> TrailingContent
-    ) {
-        self._text = text
-        self.placeholder = placeholder
-        self.shouldChange =  { _ in true }
-        self.leadingItem = leadingItem()
-        self.trailingItem = trailingItem()
-    }
-    
-    public init(
-        text: Binding<String>,
-        placeholder: String,
-        @ViewBuilder leadingItem: () -> LeadingContent
-    ) where TrailingContent == EmptyView {
-        self._text = text
-        self.placeholder = placeholder
-        self.shouldChange =  { _ in true }
-        self.leadingItem = leadingItem()
-        self.trailingItem = EmptyView()
-    }
-    
-    public init(
-        text: Binding<String>,
-        placeholder: String,
-        @ViewBuilder trailingItem: () -> TrailingContent
-    ) where LeadingContent == EmptyView {
-        self._text = text
-        self.placeholder = placeholder
-        self.shouldChange =  { _ in true }
-        self.leadingItem = EmptyView()
-        self.trailingItem = trailingItem()
-    }
-    
-    public init(
-        text: Binding<String>,
-        placeholder: String,
-        shouldChange: @escaping @Sendable @MainActor (String) -> Bool,
-        @ViewBuilder leadingItem: () -> LeadingContent,
-        @ViewBuilder trailingItem: () -> TrailingContent
-    ) {
-        self._text = text
-        self.placeholder = placeholder
-        self.shouldChange = shouldChange
-        self.leadingItem = leadingItem()
-        self.trailingItem = trailingItem()
-    }
-    
-    public init(
-        text: Binding<String>,
-        placeholder: String
-    ) where LeadingContent == EmptyView, TrailingContent == EmptyView {
-        self._text = text
-        self.placeholder = placeholder
-        self.shouldChange =  { _ in true }
-        self.leadingItem = EmptyView()
-        self.trailingItem = EmptyView()
-    }
+	let leadingItem: LeadingContent
+	let trailingItem: TrailingContent
+	
+	private var caretColor: UIColor = .black
+	
+	public init(
+		text: Binding<String>,
+		placeholder: String,
+		@ViewBuilder leadingItem: () -> LeadingContent,
+		@ViewBuilder trailingItem: () -> TrailingContent
+	) {
+		self._text = text
+		self.placeholder = placeholder
+		self.shouldChange =  { _ in true }
+		self.leadingItem = leadingItem()
+		self.trailingItem = trailingItem()
+	}
+	
+	public init(
+		text: Binding<String>,
+		placeholder: String,
+		@ViewBuilder leadingItem: () -> LeadingContent
+	) where TrailingContent == EmptyView {
+		self._text = text
+		self.placeholder = placeholder
+		self.shouldChange =  { _ in true }
+		self.leadingItem = leadingItem()
+		self.trailingItem = EmptyView()
+	}
+	
+	public init(
+		text: Binding<String>,
+		placeholder: String,
+		@ViewBuilder trailingItem: () -> TrailingContent
+	) where LeadingContent == EmptyView {
+		self._text = text
+		self.placeholder = placeholder
+		self.shouldChange =  { _ in true }
+		self.leadingItem = EmptyView()
+		self.trailingItem = trailingItem()
+	}
+	
+	public init(
+		text: Binding<String>,
+		placeholder: String,
+		shouldChange: @escaping @Sendable @MainActor (String) -> Bool,
+		@ViewBuilder leadingItem: () -> LeadingContent,
+		@ViewBuilder trailingItem: () -> TrailingContent
+	) {
+		self._text = text
+		self.placeholder = placeholder
+		self.shouldChange = shouldChange
+		self.leadingItem = leadingItem()
+		self.trailingItem = trailingItem()
+	}
+	
+	public init(
+		text: Binding<String>,
+		placeholder: String
+	) where LeadingContent == EmptyView, TrailingContent == EmptyView {
+		self._text = text
+		self.placeholder = placeholder
+		self.shouldChange =  { _ in true }
+		self.leadingItem = EmptyView()
+		self.trailingItem = EmptyView()
+	}
 	
 	public var body: some View {
-        HStack(alignment: .center, spacing: .x1) {
-            leadingItem
+		HStack(alignment: .center, spacing: .x1) {
+			leadingItem
 			TextFieldRepresentable(
 				text: $text,
 				placeholder: placeholder,
+				tintColor: caretColor,
 				shouldChange: shouldChange
 			)
-            .frame(height: inputViewStyle.height)
-            trailingItem
-            if !text.isEmpty {
-                Button {
-                    text = ""
-                } label: {
-                    Image(systemName: "xmark.app.fill")
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.black, .background.secondary)
-                        .font(.body)
-                }
-            }
+			.frame(height: inputViewStyle.height)
+			
+			trailingItem
+			if !text.isEmpty {
+				Button {
+					text = ""
+				} label: {
+					Image(systemName: "xmark.app.fill")
+						.symbolRenderingMode(.palette)
+						.foregroundStyle(.black, .background.secondary)
+						.font(.body)
+				}
+			}
 		}
-        .padding(.horizontal, .x2)
+		.padding(.horizontal, .x2)
 		.background(in: RoundedRectangle(cornerRadius: 24))
-		.backgroundStyle(.black.opacity(0.04))
-        .foregroundStyle(.primary)
+		.backgroundStyle(Color(.secondaryGray))
+	}
+	
+	public func caretColor(_ color: UIColor) -> InputView<LeadingContent, TrailingContent> {
+		var copy = self
+		copy.caretColor = color
+		return copy
 	}
 }
 
 #Preview {
-    @Previewable @State var text: String = ""
-    InputView(
-        text: .constant(""),
-        placeholder: "LOL",
-        leadingItem: {
-            Image(systemName: "rectangle.3.offgrid")
-        }
-    )
-    
-    InputView(
-        text: .constant(""),
-        placeholder: "LOL",
-        trailingItem: {
-            Image(systemName: "rectangle.3.offgrid")
-        }
-    )
-    
-    InputView(
-        text: .constant("some text..."),
-        placeholder: "LOL",
-        leadingItem: {
-            Image(systemName: "rectangle.3.offgrid")
-        },
-        trailingItem: {
-            Image(systemName: "rectangle.3.offgrid")
-        }
-    )
-    
-    InputView(
-        text: $text,
-        placeholder: "LOL",
-        leadingItem: {
-            Image(systemName: "rectangle.3.offgrid")
-        },
-        trailingItem: {
-            Image(systemName: "rectangle.3.offgrid")
-        }
-    )
-    .inputViewStyle(.large)
-
+	@Previewable @State var text: String = ""
+	InputView(
+		text: .constant(""),
+		placeholder: "LOL",
+		leadingItem: {
+			Image(systemName: "rectangle.3.offgrid")
+		}
+	)
+	
+	InputView(
+		text: .constant(""),
+		placeholder: "LOL",
+		trailingItem: {
+			Image(systemName: "rectangle.3.offgrid")
+		}
+	)
+	.caretColor(.red)
+	
+	InputView(
+		text: .constant("some text..."),
+		placeholder: "LOL",
+		leadingItem: {
+			Image(systemName: "rectangle.3.offgrid")
+		},
+		trailingItem: {
+			Image(systemName: "rectangle.3.offgrid")
+		}
+	)
+	
+	InputView(
+		text: $text,
+		placeholder: "LOL",
+		leadingItem: {
+			Image(systemName: "rectangle.3.offgrid")
+		},
+		trailingItem: {
+			Image(systemName: "rectangle.3.offgrid")
+		}
+	)
+	.inputViewStyle(.large)
 }
--- Mokay/Mokay/DesignKit/InputView/TextFieldRepresentable.swift
+++ Mokay/Mokay/DesignKit/InputView/TextFieldRepresentable.swift
@@ -8,106 +8,117 @@
 import SwiftUI
 
 @MainActor struct TextFieldRepresentable: UIViewRepresentable {
-    
-    @Binding var text: String
-    private var placeholder: String
-    private let shouldChange: (String) -> Bool
+	
+	@Binding var text: String
+	private var placeholder: String
+	private let shouldChange: (String) -> Bool
+	private let tintColor: UIColor?
 
-    init(
-        text: Binding<String>,
-        placeholder: String,
-        shouldChange: @escaping @MainActor (String) -> Bool
-    ) {
-        _text = text
-        self.placeholder = placeholder
-        self.shouldChange = shouldChange
-    }
-    
-    public func makeUIView(context: Context) -> UITextField {
-        let textField = UITextField()
-        textField.delegate = context.coordinator
-        textField.placeholder = placeholder
-        textField.textAlignment = .left
-        switch context.environment.inputViewStyle {
-        case .medium:
-            textField.font = .typography(.body)
-        case .large:
-            textField.font = .typography(.h3)
-        }
-        textField.tintColor = .black
-        textField.addTarget(
-            context.coordinator,
-            action: #selector(Coordinator.textFieldEditingChanged),
-            for: .editingChanged
-        )
-        textField.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-        return textField
-    }
-    
-    public func updateUIView(_ uiView: UITextField, context: Context) {
-        uiView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-        uiView.text = text
-    }
-    
-    public func makeCoordinator() -> Coordinator {
-        return Coordinator(
-            text: $text,
-            shouldChange: shouldChange
-        )
-    }
-    
-    @MainActor public final class Coordinator: NSObject {
-        
-        @Binding private var text: String
-        private let shouldChange: (String) -> Bool
-        private let textTransform: [StringTransform]
-        
-        init(
-            text: Binding<String>,
-            textTransform: [StringTransform] = [.toLatin, .stripCombiningMarks],
-            shouldChange: @escaping (String) -> Bool
-        ) {
-            self._text = text
-            self.textTransform = textTransform
-            self.shouldChange = shouldChange
-        }
-        
-        @objc func textFieldEditingChanged(textField: UITextField) {
-            text = textField.text ?? ""
-        }
-    }
-    
+	init(
+		text: Binding<String>,
+		placeholder: String,
+		tintColor: UIColor? = nil,
+		shouldChange: @escaping @MainActor (String) -> Bool
+	) {
+		_text = text
+		self.placeholder = placeholder
+		self.shouldChange = shouldChange
+		self.tintColor = tintColor
+	}
+	
+	public func makeUIView(context: Context) -> UITextField {
+		let textField = UITextField()
+		textField.delegate = context.coordinator
+		textField.placeholder = placeholder
+		textField.textColor = .textPrimary
+		textField.textAlignment = .left
+		textField.tintColor = tintColor ?? .black
+		textField.attributedPlaceholder = NSAttributedString(
+			string: placeholder,
+			attributes: [
+				.foregroundColor: UIColor.textSecondary
+			]
+		)
+		switch context.environment.inputViewStyle {
+		case .medium:
+			textField.font = .typography(.body)
+		case .large:
+			textField.font = .typography(.h3)
+		}
+		textField.addTarget(
+			context.coordinator,
+			action: #selector(Coordinator.textFieldEditingChanged),
+			for: .editingChanged
+		)
+		textField.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+		return textField
+	}
+	
+	public func updateUIView(_ uiView: UITextField, context: Context) {
+		uiView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+		uiView.text = text
+		uiView.tintColor = tintColor ?? .black
+	}
+	
+	public func makeCoordinator() -> Coordinator {
+		return Coordinator(
+			text: $text,
+			shouldChange: shouldChange
+		)
+	}
+	
+	@MainActor public final class Coordinator: NSObject {
+		
+		@Binding private var text: String
+		private let shouldChange: (String) -> Bool
+		private let textTransform: [StringTransform]
+		
+		init(
+			text: Binding<String>,
+			textTransform: [StringTransform] = [.toLatin, .stripCombiningMarks],
+			shouldChange: @escaping (String) -> Bool
+		) {
+			self._text = text
+			self.textTransform = textTransform
+			self.shouldChange = shouldChange
+		}
+		
+		@objc func textFieldEditingChanged(textField: UITextField) {
+			text = textField.text ?? ""
+		}
+	}
+	
 }
 
 extension TextFieldRepresentable.Coordinator: UITextFieldDelegate {
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard let text = textField.text,
-              let replacedRange = Range(range, in: text) else {
-            return false
-        }
-        
-        let replacement = transform(text: string, textTransform: textTransform)
-        let displayedText = text.replacingCharacters(in: replacedRange, with: replacement)
-        
-        let shouldChangeCharacters = shouldChange(displayedText)
-        let needsForceReplace = shouldChangeCharacters && replacement != string
-        
-        if needsForceReplace,
-           let replaceStart = textField.position(from: textField.beginningOfDocument, offset: range.location),
-           let replaceEnd = textField.position(from: replaceStart, offset: range.length),
-           let textRange = textField.textRange(from: replaceStart, to: replaceEnd) {
-            textField.replace(textRange, withText: replacement)
-            
-            return false
-        }
-        
-        return shouldChangeCharacters
-    }
-    
-    private func transform(text: String, textTransform: [StringTransform]) -> String {
-        return textTransform.reduce(text) { partialResult, transform in
-            partialResult.applyingTransform(transform, reverse: false) ?? partialResult
-        }
-    }
+	
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+		guard let text = textField.text,
+			  let replacedRange = Range(range, in: text) else {
+			return false
+		}
+		
+		let replacement = transform(text: string, textTransform: textTransform)
+		let displayedText = text.replacingCharacters(in: replacedRange, with: replacement)
+		
+		let shouldChangeCharacters = shouldChange(displayedText)
+		let needsForceReplace = shouldChangeCharacters && replacement != string
+		
+		if needsForceReplace,
+		   let replaceStart = textField.position(from: textField.beginningOfDocument, offset: range.location),
+		   let replaceEnd = textField.position(from: replaceStart, offset: range.length),
+		   let textRange = textField.textRange(from: replaceStart, to: replaceEnd) {
+			textField.replace(textRange, withText: replacement)
+			
+			return false
+		}
+		
+		return shouldChangeCharacters
+	}
+	
+	private func transform(text: String, textTransform: [StringTransform]) -> String {
+		return textTransform.reduce(text) { partialResult, transform in
+			partialResult.applyingTransform(transform, reverse: false) ?? partialResult
+		}
+	}
 }
